3582f489a45df346c8c8ea5b36a0bcfb62b2ef04
john carvajal <john.carvajal1074@gmail.com>
2024-01-31 17:36:10 -0500

    Agregar archivos de proyecto.

---
+++ SuperHeroesApp/Models/SuperHeroe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroesApp.Models
+{
+    class SuperHeroe
+    {
+        public int id;
+        public string Nombre;
+        public string IdentSecreta;
+        public string CiudadOrigen;
+        public List<SuperPoder> SuperPoderes = new List<SuperPoder>();
+        public bool PuedeVolar;
+
+        public SuperHeroe()
+        {
+            id = 1;
+            SuperPoderes = new List<SuperPoder>();
+            PuedeVolar = false;
+        }
+
+        public string UsarSuperPoderes()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in SuperPoderes)
+            {
+                sb.AppendLine($"{Nombre} esta usando el poder {item.Nombre}!!!");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+}
---
+++ SuperHeroesApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using SuperHeroesApp.Models;
+
+var poderVolar = new SuperPoder();
+poderVolar.Nombre = "volar";
+poderVolar.Descripcion = "capacidad de volar y planear";
+poderVolar.Nivel = NivelPoder.NivelDos;
+
+var superFuerza = new SuperPoder();
+superFuerza.Nombre = "super fuerza";
+superFuerza.Nivel = NivelPoder.NivelTRes;
+
+var superman = new SuperHeroe();
+superman.id = 1;
+superman.Nombre = "superman";
+superman.IdentSecreta = "clark kent";
+superman.CiudadOrigen = "metropolis";
+superman.PuedeVolar = true;
+
+var superman2 = new SuperHeroe();
+superman2.id = 1;
+superman2.Nombre = "superman";
+superman2.IdentSecreta = "clark kent";
+superman2.CiudadOrigen = "metropolis";
+superman2.PuedeVolar = true;
+
+Console.WriteLine(superman == superman2);
+
+SuperHeroeRecord superHeroRecord = new SuperHeroeRecord(1, "Superman", "Clark Kent");
+SuperHeroeRecord superHeroRecord2 = new SuperHeroeRecord(1, "Superman", "Clark Kent");
+
+Console.WriteLine(superHeroRecord == superHeroRecord2);
+
+//List<SuperPoder> poderesSuperman = new List<SuperPoder>();
+//poderesSuperman.Add(poderVolar);
+//poderesSuperman.Add(superFuerza);
+//superman.SuperPoderes = poderesSuperman;
+//string resultSuperPoderes = superman.UsarSuperPoderes();
+//Console.WriteLine(resultSuperPoderes);
+
+enum NivelPoder
+{
+    NivelUno,
+    NivelDos,
+    NivelTRes,
+}
+
+public record SuperHeroeRecord(int id, string Nombre, string IdentidadSecreta);
